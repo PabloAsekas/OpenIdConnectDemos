21de484063b2106240d69f742ae92de605322229
Manuel Vilachán <mvilachan@gmail.com>
2018-11-21 14:13:40 +0100

    Initial commit with demo code

---
+++ Host.Advanced/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Host.Advanced.Configuration;
+using Host.Advanced.Data;
+using Host.Advanced.Models;
+using Host.Advanced.Services;
+using Host.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Host.Advanced
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                options.UseLazyLoadingProxies();
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+                .AddInMemoryApiResources(Resources.GetApiResources())
+                .AddInMemoryClients(Clients.Get())
+                .AddAspNetIdentity<ApplicationUser>();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = "813017584167-dqd2qoo9oau2khmg0binpefnjgq5udar.apps.googleusercontent.com";
+                    options.ClientSecret = "Vh0P8HMQbIDJ4E2PK5XEkiQj";
+                })
+                .AddMicrosoftAccount(options =>
+                {
+                    options.ClientId = "2d8b1dbd-7bb4-4098-b974-541523a01565";
+                    options.ClientSecret = "pbmsoaSCU59625@^mPNHJ^+";
+                });
+
+            services.Configure<U2FManagerOptions>(options => options.AppUri = new Uri(Configuration.GetValue<string>("IdSrvUrl")));
+            services.AddScoped<IU2FManagerService, U2FManagerService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseAuthentication(); // UseAuthentication not needed -- UseIdentityServer add this
+            app.UseIdentityServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # OpenIdConnectDemos
+
+This project contains the demos explained in the event Commit 2018 Madrid.
+
+Every web host is based in the standard template _ASP.NET Core Web Application_ within Visual Studio.
+
+Before running the projects, a couple of steps should be done:
+
+1. Create the database for each project (Host & Host.Advance). Execute the following command from a console within each directory:
+```
+dotnet ef database update -c ApplicationDbContext
+```
+2. Seed each the database with users data. The parameter seed will be catched in the Program entry point. Execute the following command from a console within each directory:
+```
+dotnet run -- seed
+```
+
+## Demo 1
+### Create our first SSO server
+
+This project will act as an identity server implementing the protocol OpenId Connect for our applications. To accomplish this we'll use the library <a href="https://github.com/IdentityServer/IdentityServer4">Identity Server 4</a> as well as <a href="https://github.com/aspnet/identity">ASP.NET Core Identity</a> available as nuget packages.
+
+The project, in addition to be based on the standard template, contains a Quickstart template from <a href="https://github.com/IdentityServer/IdentityServer4.Samples">IS4 Samples</a> repo.
+
+The relevant code for this demo is in Startup class where the everything is configured.
